f972b0f504d3cbf02c224d80215208d3d6649dc0
mcortezv <mdjesuscv@gmail.com>
2026-02-18 08:55:30 -0700

    Atributos Clases Dominio

--- uno-spin-common/src/main/java/domain/Descarte.java
+++ uno-spin-common/src/main/java/domain/Descarte.java
@@ -1,4 +1,41 @@
 package domain;
+import java.util.List;
 
+/**
+ * The type Descarte.
+ */
 public class Descarte {
+    private List<Carta> cartas;
+
+    /**
+     * Instantiates a new Descarte.
+     */
+    public Descarte() {}
+
+    /**
+     * Instantiates a new Descarte.
+     *
+     * @param cartas the cartas
+     */
+    public Descarte(List<Carta> cartas) {
+        this.cartas = cartas;
+    }
+
+    /**
+     * Gets cartas.
+     *
+     * @return the cartas
+     */
+    public List<Carta> getCartas() {
+        return cartas;
+    }
+
+    /**
+     * Sets cartas.
+     *
+     * @param cartas the cartas
+     */
+    public void setCartas(List<Carta> cartas) {
+        this.cartas = cartas;
+    }
 }
--- uno-spin-common/src/main/java/domain/Mano.java
+++ uno-spin-common/src/main/java/domain/Mano.java
@@ -1,4 +1,41 @@
 package domain;
+import java.util.List;
 
+/**
+ * The type Mano.
+ */
 public class Mano {
+    private List<Carta> cartas;
+
+    /**
+     * Instantiates a new Mano.
+     */
+    public Mano() {}
+
+    /**
+     * Instantiates a new Mano.
+     *
+     * @param cartas the cartas
+     */
+    public Mano(List<Carta> cartas) {
+        this.cartas = cartas;
+    }
+
+    /**
+     * Gets cartas.
+     *
+     * @return the cartas
+     */
+    public List<Carta> getCartas() {
+        return cartas;
+    }
+
+    /**
+     * Sets cartas.
+     *
+     * @param cartas the cartas
+     */
+    public void setCartas(List<Carta> cartas) {
+        this.cartas = cartas;
+    }
 }
