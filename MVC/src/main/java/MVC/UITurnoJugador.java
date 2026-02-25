82dcc1e574a7c4b8cb4b8d1c722a87ff06e3f36c
SebastianEsc05 <sebastianescram01@gmail.com>
2026-02-25 14:13:00 -0700

    jugador dto

--- MVC/src/main/java/MVC/UITurnoJugador.java
+++ MVC/src/main/java/MVC/UITurnoJugador.java
@@ -1,18 +1,28 @@
 package MVC;
 import MVC.interfaces.IComponent;
+import MVC.interfaces.IModeloLectura;
+import MVC.interfaces.ISuscriptor;
+
 import javax.swing.*;
 
-public class UITurnoJugador extends JFrame implements IComponent {
-    private Controlador controlador;
+public class UITurnoJugador extends JFrame implements IComponent, ISuscriptor {
+    private final Controlador controlador;
+    private final IModeloLectura lectura;
     private UITablero tablero;
     private UIMano mano;
     private UIDescarte descarte;
     private UIMazo mazo;
 
-    public UITurnoJugador(Controlador controlador) {
+    public UITurnoJugador(Controlador controlador, IModeloLectura modeloLectura) {
         this.controlador = controlador;
+        this.lectura = modeloLectura;
     }
 
     @Override
     public void execute(){}
+
+    @Override
+    public void update(IModeloLectura modelo) {
+
+    }
 }
