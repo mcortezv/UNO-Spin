d837ee3a7b92d47dfd7fa21d2cca699c70988dc0
SebastianEsc05 <sebastianescram01@gmail.com>
2026-03-13 03:52:56 -0700

    creacion clases de diagrama de conexion

---
+++ src/main/java/mvc/comunicacion/SocketOUT.java
@@ -0,0 +1,23 @@
+package mvc.comunicacion;
+
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+
+public class SocketOUT {
+    private ExecutorService worker;
+    private Socket socket;
+
+    public void start(){
+
+    }
+
+    public void close(){
+
+    }
+
+    public void update(String json, int port, String ip){
+
+    }
+
+
+}
