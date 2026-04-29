f62834af61d49dcba28e810cb249bee97127a4cd
mcortezv <mdjesuscv@gmail.com>
2026-04-28 23:38:29 -0700

    update: implementacion y factory de conexion

--- conexion/src/main/java/conexion/Conexion.java
+++ conexion/src/main/java/conexion/Conexion.java
@@ -1,17 +1,52 @@
 package conexion;
+import dispatcher.ColaDispatcher;
+import dispatcher.Dispatcher;
+import dispatcher.SocketOut;
+import interfaces.IDispatcher;
 import interfaces.IReceptor;
+import interfaces.IReceptorObserver;
+import receptor.ColaReceptor;
+import receptor.SocketIn;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Conexion {
+public class Conexion implements IReceptorObserver {
     private static Conexion instance;
     private final List<IReceptor> suscriptores = new ArrayList<>();
+    private final SocketIn socketIn;
+    private final SocketOut socketOut;
+    private final IDispatcher dispatcher;
 
-    Conexion() {
+    Conexion(int puerto) {
+        ColaReceptor colaReceptor = new ColaReceptor();
+        colaReceptor.attach(this);
+        this.socketIn = new SocketIn(puerto, colaReceptor);
+
+        ColaDispatcher colaDispatcher = new ColaDispatcher();
+        this.socketOut = new SocketOut();
+        colaDispatcher.attach(socketOut);
+        this.dispatcher = new Dispatcher(colaDispatcher);
+
         instance = this;
     }
 
-    public static void suscribir (IReceptor receptor){
+    public static void iniciar() {
+        instance.socketOut.start();
+        instance.socketIn.start();
+    }
+
+    public static void suscribir(IReceptor receptor) {
         instance.suscriptores.add(receptor);
+    }
+
+    public IDispatcher getDispatcher() {
+        return dispatcher;
+    }
+
+    @Override
+    public void update(String json, int port, String ip) {
+        for (IReceptor s : suscriptores) {
+            s.recibirMensaje(json);
+        }
     }
 }
--- conexion/src/main/java/factory/FactoryConexion.java
+++ conexion/src/main/java/factory/FactoryConexion.java
@@ -1,11 +1,13 @@
 package factory;
+import conexion.Conexion;
 import interfaces.IDispatcher;
 import interfaces.IFactoryConexion;
 
 public class FactoryConexion implements IFactoryConexion {
 
     @Override
-    public IDispatcher crearConn() {
-        return null;
+    public IDispatcher crearConn(int puerto) {
+        Conexion conexion = new Conexion(puerto);
+        return conexion.getDispatcher();
     }
 }
