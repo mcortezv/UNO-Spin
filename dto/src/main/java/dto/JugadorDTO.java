f86f6a9521de239adba249a5c04db9345756d91f
mcortezv <mdjesuscv@gmail.com>
2026-04-29 00:02:55 -0700

    update: eliminando leak de cartas rival

--- dto/src/main/java/dto/JugadorDTO.java
+++ dto/src/main/java/dto/JugadorDTO.java
@@ -1,78 +1,49 @@
 package dto;
 
-import java.util.List;
-
-/**
- * The type Jugador dto.
- */
 public class JugadorDTO {
     private String nombre;
     private int numeroAvatar;
     private int cantidadCartas;
     private boolean esTurnoActual;
-    private List<CartaDTO> cartasMano;
 
-    /**
-     * Instantiates a new Jugador dto.
-     */
-    public JugadorDTO(){}
+    public JugadorDTO() {}
 
-    /**
-     * Instantiates a new Jugador dto.
-     *
-     * @param nombre         the nombre
-     * @param numeroAvatar   the numero avatar
-     * @param cantidadCartas the cantidad cartas
-     * @param esTurnoActual  the es turno actual
-     */
-    public JugadorDTO(String nombre, int numeroAvatar, int cantidadCartas, boolean esTurnoActual, List<CartaDTO> cartasMano) {
+    public JugadorDTO(String nombre, int numeroAvatar, int cantidadCartas, boolean esTurnoActual) {
         this.nombre = nombre;
         this.numeroAvatar = numeroAvatar;
         this.cantidadCartas = cantidadCartas;
         this.esTurnoActual = esTurnoActual;
-        this.cartasMano = cartasMano;
     }
 
-    /**
-     * Gets nombre.
-     *
-     * @return the nombre
-     */
     public String getNombre() {
-       return nombre;}
+        return nombre;
+    }
 
-    /**
-     * Gets cantidad cartas.
-     *
-     * @return the cantidad cartas
-     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public int getCantidadCartas() {
-       return cantidadCartas;
-   }
+        return cantidadCartas;
+    }
 
-    /**
-     * Gets numero avatar.
-     *
-     * @return the numero avatar
-     */
+    public void setCantidadCartas(int cantidadCartas) {
+        this.cantidadCartas = cantidadCartas;
+    }
+
     public int getNumeroAvatar() {
         return numeroAvatar;
-   }
-
-    /**
-     * Is es turno actual boolean.
-     *
-     * @return the boolean
-     */
-    public boolean isEsTurnoActual() {
-       return esTurnoActual;
-   }
-
-    public List<CartaDTO> getCartasMano() {
-        return cartasMano;
     }
 
-    public void setCartasMano(List<CartaDTO> cartasMano) {
-        this.cartasMano = cartasMano;
+    public void setNumeroAvatar(int numeroAvatar) {
+        this.numeroAvatar = numeroAvatar;
+    }
+
+    public boolean isEsTurnoActual() {
+        return esTurnoActual;
+    }
+
+    public void setEsTurnoActual(boolean esTurnoActual) {
+        this.esTurnoActual = esTurnoActual;
     }
 }
