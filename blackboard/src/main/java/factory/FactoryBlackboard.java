96c3b73bc3fbf64dc34871c68c2622e8b202f046
Majo8805 <maria.enriquez252337@potros.itson.edu.mx>
2026-05-01 21:22:05 -0700

    Blackboard y su Factory

--- blackboard/src/main/java/factory/FactoryBlackboard.java
+++ blackboard/src/main/java/factory/FactoryBlackboard.java
@@ -1,15 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package factory;
-
+import dominio.entidades.Jugador;
+import dominio.entidades.Partida;
+import dominio.IDominio;
+import dominio.entidades.enums.EstadoPartida;
+import interfaces.IBlackboard;
 import interfaces.IFactoryBlackboard;
+import interfaces.IReceptor;
+import interfaces.ISerializer;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- *
- * @author janethcristinagalvanquinonez
+ * The type Factory blackboard.
  */
-public class FactoryBlackboard implements IFactoryBlackboard{
-    
+public class FactoryBlackboard implements IFactoryBlackboard {
+    private Blackboard blackboard;
+
+    private Blackboard crearBlackboard(ISerializer serializer){
+        if (blackboard == null){
+            EstadoPartida estadoPartida = EstadoPartida.NO_INICIADA;
+            int indiceJugadorActual = 0;
+            List<Jugador> jugadores = new ArrayList<>();
+            boolean sentidoHorario = true;
+
+            IDominio dominio = new Partida(estadoPartida, indiceJugadorActual, jugadores, sentidoHorario);
+            this.blackboard = new Blackboard(dominio, serializer);
+        }
+        return blackboard;
+    }
+
+    @Override
+    public IBlackboard crearBlackboardObservable(ISerializer serializer) {
+        return crearBlackboard(serializer);
+    }
+
+    @Override
+    public IReceptor crearReceptorBlackboard(ISerializer serializer) {
+        return crearBlackboard(serializer);
+    }
 }
