b9de9b8a0ea52174ff7b78bf1be1ba694d348d0e
Dana Rios <117124094+LU-12-12@users.noreply.github.com>
2026-05-02 00:57:27 -0700

    Conexion-Dispatcher
    Paquete Dispatcher del modulo de conexión

--- conexion/src/main/java/dispatcher/Dispatcher.java
+++ conexion/src/main/java/dispatcher/Dispatcher.java
@@ -4,16 +4,24 @@ import interfaces.IDispatcherObserver;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The type Dispatcher.
+ */
 public class Dispatcher implements IDispatcher {
-    private final List<IDispatcherObserver> observadores= new ArrayList<>();
+    private final List<IDispatcherObserver> observadores = new ArrayList<>();
 
-   public void attach(IDispatcherObserver observador){
-       observadores.add(observador);
-   }
+    /**
+     * Attach.
+     *
+     * @param observador the observador
+     */
+    public void attach(IDispatcherObserver observador) {
+        observadores.add(observador);
+    }
 
     @Override
     public void enviar(String json, int port, String ip) {
-        for(IDispatcherObserver obs: observadores){
+        for (IDispatcherObserver obs : observadores) {
             obs.update(json, port, ip);
         }
     }
