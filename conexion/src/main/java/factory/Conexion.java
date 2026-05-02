af51c94f9fced842174b4caf0b967d6bf83e94df
Dana Rios <117124094+LU-12-12@users.noreply.github.com>
2026-05-02 00:59:39 -0700

    conexion-factory
    Paquete factory del modulo de conexion

--- conexion/src/main/java/factory/Conexion.java
+++ conexion/src/main/java/factory/Conexion.java
@@ -1,13 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package factory;
 
+import dispatcher.Dispatcher;
+import dispatcher.SocketOut;
+import interfaces.IDispatcher;
+import interfaces.IReceptor;
+import interfaces.IReceptorObserver;
+import receptor.SocketIn;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
- * @author janethcristinagalvanquinonez
+ * Dana Rios
  */
-public class Conexion {
-    
+public class Conexion implements IReceptorObserver {
+
+    private static Conexion instance;
+    private final List<IReceptor> suscriptores = new ArrayList<>();
+    private final SocketIn socketIn;
+    private final SocketOut socketOut;
+    private final IDispatcher dispatcher;
+
+    /**
+     * Instantiates a new Conexion.
+     *
+     * @param puerto the puerto
+     */
+    Conexion(int puerto) {
+        this.socketIn = new SocketIn(puerto, this);
+
+        Dispatcher dispatcher = new Dispatcher();
+        this.socketOut = new SocketOut();
+        dispatcher.attach(socketOut);
+        this.dispatcher = dispatcher;
+
+        instance = this;
+    }
+
+    /**
+     * Iniciar.
+     */
+    public static void iniciar() {
+        instance.socketOut.start();
+        instance.socketIn.start();
+    }
+
+    /**
+     * Suscribir.
+     *
+     * @param receptor the receptor
+     */
+    public static void suscribir(IReceptor receptor) {
+        instance.suscriptores.add(receptor);
+    }
+
+    /**
+     * Gets dispatcher.
+     *
+     * @return the dispatcher
+     */
+    public IDispatcher getDispatcher() {
+        return dispatcher;
+    }
+
+    @Override
+    public void update(String json, int port, String ip) {
+        for (IReceptor s : suscriptores) {
+            s.recibirMensaje(json);
+        }
+    }
 }
