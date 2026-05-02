47da6660a8c2b3f1f0a5182c94aeda02ca9df743
Dana Rios <117124094+LU-12-12@users.noreply.github.com>
2026-05-02 01:22:16 -0700

    DTO - TipoEventoRuletaDTO
    parte 2

---
+++ dto/src/main/java/dto/TipoEventoRuletaDTO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+//Dana Rios
+public enum TipoEventoRuletaDTO {
+    CASI_UNO,
+    DESCARTAR_POR_NUMERO,
+    DESCARTAR_POR_COLOR,
+    ROBAR_HASTA_AZUL,
+    ROBAR_HASTA_ROJO,
+    GUERRA,
+    MOSTRAR_LA_MANO,
+    INTERCAMBIO_DE_MANOS,
+    PUNTUACION_MAS_BAJA
+}
